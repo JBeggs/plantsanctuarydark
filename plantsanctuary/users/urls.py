374484b2819c45edbf43ae250b9fd741cd90fd15
Jody <jody.beggs@cullinansystems.co.za>
2023-04-26 14:34:20 +0200

    updated dark theme

---
+++ plantsanctuary/users/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    AddressViewSet,
+    ProfileAPIView,
+    SendOrResendSMSAPIView,
+    UserAPIView,
+    VerifyPhoneNumberAPIView,
+)
+
+app_name = 'users'
+
+router = DefaultRouter()
+router.register(r'', AddressViewSet)
+
+urlpatterns = [
+
+    path(
+        'send-sms/',
+        SendOrResendSMSAPIView.as_view(),
+        name='send_resend_sms'
+    ),
+    path(
+        'verify-phone/',
+        VerifyPhoneNumberAPIView.as_view(),
+        name='verify_phone_number'
+    ),
+
+    path('', UserAPIView.as_view(), name='user_detail'),
+    path('profile/', ProfileAPIView.as_view(), name='profile_detail'),
+    path('profile/address/', include(router.urls)),
+
+]
